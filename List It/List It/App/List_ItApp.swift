506fa6cca9cfe42afa00f4c573cd3d78b456bd87
Abdul Moiz <abdul.m01z10123@gmail.com>
2025-03-30 12:47:03 +0100

    Created iOS Project

---
+++ List It/List It/App/List_ItApp.swift
@@ -0,0 +1,17 @@
+//
+//  List_ItApp.swift
+//  List It
+//
+//  Created by Abdul Moiz on 30/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct List_ItApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ List It/List ItTests/List_ItTests.swift
@@ -0,0 +1,17 @@
+//
+//  List_ItTests.swift
+//  List ItTests
+//
+//  Created by Abdul Moiz on 30/03/2025.
+//
+
+import Testing
+@testable import List_It
+
+struct List_ItTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
